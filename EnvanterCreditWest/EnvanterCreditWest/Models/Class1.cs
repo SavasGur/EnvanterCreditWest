250a89cb27ae3f0fafba28b941f6a0bca962e4c6
asli.kirmizi <asli.kirmizi@CWKREW143NEW>
2018-07-16 10:13:04 +0300

    hey

--- EnvanterCreditWest/EnvanterCreditWest/Models/Class1.cs
+++ EnvanterCreditWest/EnvanterCreditWest/Models/Class1.cs
@@ -5,19 +5,7 @@ using System.Web;
 
 namespace EnvanterCreditWest.Models
 {
-    public class Products
-    {
-        public int Id { get; set; }
-        public string Type { get; set; }
-        public string Model { get; set; }
-        public string Brand { get; set; }
-        public DateTime WarrantyEnd { get; set; }
-        public DateTime DateAcquired { get; set; }
-        public int Barcode { get; set; }
-        public DateTime ProductionDate { get; set; }
-        public string Condition { get; set; }
-        public int Price { get; set; }
-    }
+
 
     public class Branches
     {
