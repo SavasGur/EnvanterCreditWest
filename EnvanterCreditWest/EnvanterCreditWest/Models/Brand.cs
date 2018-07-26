74698003f1ffa5d82f1fd422e22edc1a996bd33d
asli.kirmizi <asli.kirmizi@CWKREW143NEW>
2018-07-26 16:06:16 +0300

    +++

---
+++ EnvanterCreditWest/EnvanterCreditWest/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnvanterCreditWest.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; }
+    }
+}
---
+++ EnvanterCreditWest/EnvanterCreditWest/Models/Type.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnvanterCreditWest.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+}
