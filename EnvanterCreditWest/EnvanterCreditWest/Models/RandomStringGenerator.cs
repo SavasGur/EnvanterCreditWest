461455556998c9aaf9e773acd73b437853200bb3
asli.kirmizi <asli.kirmizi@CWKREW143NEW>
2018-07-31 09:40:32 +0300

    buttons done

---
+++ EnvanterCreditWest/EnvanterCreditWest/Models/RandomStringGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnvanterCreditWest.Models
+{
+    public class RandomStringGenerator
+    {
+        static Random random = new Random();
+        public static string RandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 13)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static string RandomInt()
+        {
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, 13)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+    public class BarcodeResult
+    {
+        public string Barcode { get; set; }
+        public string Error { get; set; }
+        public string Url { get; set; }
+    }
+}
