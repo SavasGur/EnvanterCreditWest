f75825a5ef84fede7db6ebf4ca714a7a1bf45889
Ozgur97 <mehmetoglu523@gmail.com>
2018-08-16 15:05:29 +0300

    Çagrııııııııııııııııı

---
+++ EnvanterCreditWest/EnvanterCreditWest/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvanterCreditWest.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+        public ActionResult Interval()
+        {
+            return View();
+        }
+    }
+}
